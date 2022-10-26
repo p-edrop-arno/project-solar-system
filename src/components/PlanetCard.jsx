c1d4f31193b9ce4ad724afa2336ec4d1ed8c1711
P-edroP-arno <pedroparno@gmail.com>
2022-10-26 11:46:16 -0300

    correção

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -5,13 +5,13 @@ class PlanetCard extends Component {
   render() {
     const { planetName } = this.props;
     const { planetImage } = this.props;
-    const alt = `Planeta${{ planetName }}`;
+    // const alt = `Planeta${{ planetName }}`;
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">
           {planetName}
         </p>
-        <img src={ planetImage } alt={ alt } />
+        <img src={ planetImage } alt="Planeta Terra" />
       </div>
     );
   }
