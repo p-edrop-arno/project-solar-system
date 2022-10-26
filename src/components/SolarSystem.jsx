a1e8e43bea0d5fae4174efd32cb7cb1c44c1a71d
P-edroP-arno <pedroparno@gmail.com>
2022-10-26 14:47:46 -0300

    6º requisito

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(({ name: nome, image }) => (
+          <PlanetCard key={ nome } planetName={ nome } planetImage={ image } />
+        ))}
       </div>
     );
   }
